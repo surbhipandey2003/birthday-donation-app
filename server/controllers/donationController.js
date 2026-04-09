8c81a74f553b801f54b45e41448933ba4b9149e3
Surbhi Pandey <surbhipandey263@gmail.com>
2026-04-09 17:21:30 +0530

    fix model name issue

--- server/controllers/donationController.js
+++ server/controllers/donationController.js
@@ -1,4 +1,4 @@
-const Donation = require("../models/Donation");
+const Donation = require("../models/donation");
 const Campaign = require("../models/Campaign");
 
 const addDonation = async (req, res) => {
